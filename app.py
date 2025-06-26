ef150f5cc1d15aff8ad02233ec2865afb805b22e
Riqueb5526p <rique10099@gmail.com>
2025-06-26 20:21:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -18,5 +19,8 @@ def index():
             resultado = "Digite um número válido!"
     return render_template("index.html", resultado=resultado)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # pega a porta do Render
+    app.run(host="0.0.0.0", port=port)
